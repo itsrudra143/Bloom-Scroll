2f8681777ade7971b2da6a295998a4a69cce36fc
Rudrakshi <rudrakshisharma86@gmail.com>
2025-01-28 14:23:33 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,64 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+
+import AnimatedTitle from "./AnimatedTitle";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const About = () => {
+  useGSAP(() => {
+    const clipAnimation = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#clip",
+        start: "center center",
+        end: "+=800 center",
+        scrub: 0.5,
+        pin: true,
+        pinSpacing: true,
+      },
+    });
+
+    clipAnimation.to(".mask-clip-path", {
+      width: "100vw",
+      height: "100vh",
+      borderRadius: 0,
+    });
+  });
+
+  return (
+    <div id="about" className="min-h-screen w-screen">
+      <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+        <p className="font-general text-sm uppercase md:text-[10px]">
+          Welcome to BloomScroll
+        </p>
+
+        <AnimatedTitle
+          title="Disc<b>o</b>ver the world's <br /> largest shared <b>a</b>dventure"
+          containerClass="mt-5 !text-black text-center"
+        />
+
+        <div className="about-subtext">
+          <p>Social media doesn’t have to overwhelm us.</p>
+          <p className="text-gray-500">
+            Lets reimagine it as a space to nurture our growth and the growth
+            of others. By curating feeds that inspire, educate, and uplift, we
+            can slow down and engage with intention. 
+          </p>
+        </div>
+      </div>
+
+      <div className="h-dvh w-screen" id="clip">
+        <div className="mask-clip-path about-image">
+          <img
+            src="img/about.jpg"
+            alt="Background"
+            className="absolute left-0 top-0 size-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Features.jsx
@@ -0,0 +1,201 @@
+import { useState, useRef } from "react";
+import { TiLocationArrow } from "react-icons/ti";
+
+export const BentoTilt = ({ children, className = "" }) => {
+  const [transformStyle, setTransformStyle] = useState("");
+  const itemRef = useRef(null);
+
+  const handleMouseMove = (event) => {
+    if (!itemRef.current) return;
+
+    const { left, top, width, height } =
+      itemRef.current.getBoundingClientRect();
+
+    const relativeX = (event.clientX - left) / width;
+    const relativeY = (event.clientY - top) / height;
+
+    const tiltX = (relativeY - 0.5) * 5;
+    const tiltY = (relativeX - 0.5) * -5;
+
+    const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(.95, .95, .95)`;
+    setTransformStyle(newTransform);
+  };
+
+  const handleMouseLeave = () => {
+    setTransformStyle("");
+  };
+
+  return (
+    <div
+      ref={itemRef}
+      className={className}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      style={{ transform: transformStyle }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const BentoCard = ({ src, title, description, isComingSoon }) => {
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+  const [hoverOpacity, setHoverOpacity] = useState(0);
+  const hoverButtonRef = useRef(null);
+
+  const handleMouseMove = (event) => {
+    if (!hoverButtonRef.current) return;
+    const rect = hoverButtonRef.current.getBoundingClientRect();
+
+    setCursorPosition({
+      x: event.clientX - rect.left,
+      y: event.clientY - rect.top,
+    });
+  };
+
+  const handleMouseEnter = () => setHoverOpacity(1);
+  const handleMouseLeave = () => setHoverOpacity(0);
+
+  return (
+    <div className="relative size-full">
+      <video
+        src={src}
+        loop
+        muted
+        autoPlay
+        className="absolute left-0 top-0 size-full object-cover object-center"
+      />
+      <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50">
+        <div>
+          <h1 className="bento-title special-font text-black">{title}</h1>
+          {description && (
+            <p className="mt-3 max-w-64 text-xs md:text-base font-bold text-black">
+              {description}
+            </p>
+          )}
+        </div>
+
+        {isComingSoon && (
+          <div
+            ref={hoverButtonRef}
+            onMouseMove={handleMouseMove}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            className="border-hsla relative flex w-fit cursor-pointer items-center gap-1 overflow-hidden rounded-full bg-black px-5 py-2 text-xs uppercase text-white/20"
+          >
+            {/* Radial gradient hover effect */}
+            <div
+              className="pointer-events-none absolute -inset-px opacity-0 transition duration-300"
+              style={{
+                opacity: hoverOpacity,
+                background: `radial-gradient(100px circle at ${cursorPosition.x}px ${cursorPosition.y}px, #656fe288, #00000026)`,
+              }}
+            />
+            <TiLocationArrow className="relative z-20" />
+            <p className="relative z-20">coming soon</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+const Features = () => (
+  <section className="bg-black pb-52">
+    <div className="container mx-auto px-3 md:px-10">
+      <div className="px-5 py-32">
+        <p className="font-circular-web text-lg text-blue-50">
+          Let your feed be a garden of positivity
+        </p>
+        <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
+          Imagine if social media became a space for growth and connection. Each
+          post could be an act of kindness, a spark of inspiration, or a step
+          toward personal growth. Lets fill this digital garden with positivity,
+          stories that empower, and content that fosters community.
+        </p>
+      </div>
+
+      {/* <BentoTilt className="border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]">
+        <BentoCard
+          src="videos/feature-1.mp4"
+          title={
+            <>
+              Mindfu<b>l</b> Engagement
+            </>
+          }
+          description="Reinventing social media starts with mindful engagement. Choose content that nurtures your growth and adds value to your day, not distractions that drain your energy"
+          isComingSoon
+        />
+      </BentoTilt> */}
+
+      <div className="grid h-[135vh] w-full grid-cols-2 grid-rows-3 gap-7">
+        <BentoTilt className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
+          <BentoCard
+            src="videos/video-1.mp4"
+            title={
+              <>
+                <p className="text-white">
+                  Purpose<b>ful</b> Connection
+                </p>
+              </>
+            }
+            description={
+              <p className="text-white">
+                Social media can be a powerful tool for connection.
+              </p>
+            }
+            isComingSoon
+          />
+        </BentoTilt>
+
+        <BentoTilt className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0 ">
+          <BentoCard
+            src="videos/video-2.mp4"
+            title={
+              <>
+                Focu<b>sed</b> Productivity
+              </>
+            }
+            description=" By curating feeds that align with your goals, you can transform your scroll into a source of motivation and personal development."
+            isComingSoon
+          />
+        </BentoTilt>
+
+        <BentoTilt className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
+          <BentoCard
+            src="videos/video-3.mp4"
+            title={
+              <>
+                Purpose<b>ful</b>Sharing
+              </>
+            }
+            description="Share content that inspires, educates, and encourages others to pursue their best selves."
+            isComingSoon
+          />
+        </BentoTilt>
+
+        <BentoTilt className="bento-tilt_2">
+          <div className="flex size-full flex-col justify-between bg-violet-300 p-5">
+            <h1 className="bento-title special-font max-w-64 text-black">
+              M<b>o</b>re co<b>m</b>ing s<b>o</b>on.
+            </h1>
+
+            <TiLocationArrow className="m-5 scale-[5] self-end" />
+          </div>
+        </BentoTilt>
+
+        <BentoTilt className="bento-tilt_2">
+          <video
+            src="videos/video-4.mp4"
+            loop
+            muted
+            autoPlay
+            className="size-full object-cover object-center"
+          />
+        </BentoTilt>
+      </div>
+    </div>
+  </section>
+);
+
+export default Features;
